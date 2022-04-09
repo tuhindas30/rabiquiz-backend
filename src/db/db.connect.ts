2e140104ea593f72ff8807769d26aac4d5caf6d8
Tuhin Das <mailme.tuhindas@gmail.com>
2022-04-09 22:34:48 +0530

    🐛 fix: update code due to outdated packages

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const initializeMongoDB = async (MONGODB_URI: string) => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("Successfully connected to MongoDB");
   } catch (error) {
     console.log("Error while connecting to MongoDB");
